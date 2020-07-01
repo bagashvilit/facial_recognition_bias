521ef9f9746deb63de6994a23f8ccc879fe71ef8
bagashvilit <bagashvilit@allegheny.edu>
2020-07-01 21:33:56 +0400

    Sort the data and move female images to the folder

--- src/sort_data.py
+++ src/sort_data.py
@@ -4,8 +4,8 @@ import os
 import cv2
 from glob import glob
 
-path = "../data/crop_part1"
 
+path = "../data/crop_part1"
 
 def preprocess_filenames(path):
     data = os.listdir(path)
@@ -46,26 +46,8 @@ def filter_file_names(age, gender, ethnicity):
 #instead of a number
 filtered  = filter_file_names('-','1', '-')
 
-
-# 
-# def filter_images(filtered_names,path):
-#     image_list = []
-#     data_path = os.path.join(path,"*.jpg")
-#     filenames = glob(data_path)
-#     for name in filtered_names:
-#         for file_name in filenames:
-#             if name in file_name:
-#                 img = cv2.imread( file_name)
-#                 image_list.append((img,name))
-#
-#     return image_list
-#     #return image_list
-#
-#
-# filtered_images = (filter_images(filtered,path))
-#
-# path_to_classified = '../data/female'
-# for i in filtered_images:
-#     print(i)
-#     for img,name in i:
-#         cv2.imwrite(os.path.join(path_to_classified , "{}".format(name)), img)
+path_to_classified = '../data/female'
+for i in filtered:
+    data_path = os.path.join(path,"{}".format(i))
+    img = cv2.imread(data_path)
+    cv2.imwrite(os.path.join(path_to_classified , "{}".format(i)), img)
