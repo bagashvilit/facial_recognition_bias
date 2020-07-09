8c27f80c330fc43fff819c1b5b2d4b4455316cf3
bagashvilit <bagashvilit@allegheny.edu>
2020-07-09 15:54:37 +0400

    Add info about the FrameWork and prototxt and caffemodel files

--- src/CNN/Convolution_neural_net.py
+++ src/CNN/Convolution_neural_net.py
@@ -1,21 +1,31 @@
+"""
+Demonstrate gender classification using Convolution nerual network.
+
+Framework: Caffe. model can also be loaded with OpenCV.
+link to the source code: https://github.com/GilLevi/AgeGenderDeepLearning
+
+"""
 import cv2
 
+
 def load_caffe_models():
+    # .prototxt file holds overall information about the neural network, such as:
+    # list of layers, connections between layers,parameters of each layers, and input/output dimensions.
+    # .caffemodel file stores weights of layers of Neural network
     gender_net = cv2.dnn.readNetFromCaffe('deploy_gender.prototxt', 'gender_net.caffemodel')
     return(gender_net)
 
 
-
 def gender_detector(image):
+
     gender_net = load_caffe_models()
 
     gender_list = ['Male', 'Female']
 
     #Preprocess image and prepare it for the classification
-    # image: the input image that needs to be preprocessed
-    # scalefactor: after mean subtraction image can be scaled by the scalefactorself.
+    # scalefactor: after mean subtraction image can be scaled by the scalefactor
     # size: size that Convolutional Neural Network expects.
-    # mean values here are for mean subtraction, usually the values that were used for
+    # mean values here are for mean subtraction, usually the mean values that were used for
     # training are used for classification as well
     mean = (104, 117, 123)
     blob = cv2.dnn.blobFromImage(image, 1.0, (227,227), mean, swapRB=False)
@@ -26,6 +36,9 @@ def gender_detector(image):
     gender = gender_list[gender_preds[0].argmax()]
     return gender
 
+# provide the path to the input image as a parameter.
+# The output will be Open CV window with a label on top, label is generated
+# using the convolution neural network
 image = cv2.imread("images/sample.jpg")
 gender = gender_detector(image)
 cv2.imshow("Gender: {}".format(gender),image)
